6830fe7fdbcf42be1c4930f6cd6bacbe7f36cf21
asdf <migsking@naver.com>
2020-04-12 22:21:46 +0900

    귗낳

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,5 +7,5 @@ def suite():
     return suite
 
 def add_test_case(suite: unittest.TestSuite):
-    from tests.services.checker import TestCheckers
-    suite.addTest(TestCheckers('test_jwt_checker'))
+    pass
+    # suite.addTest()
--- tests/services/checker.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from tenderloin.app.services import jwt_checker, user_type_checker
-
-class TestCheckers(unittest.TestCase):
-    def test_jwt_checker(self):
-        pass
